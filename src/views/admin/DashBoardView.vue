6a7e73ca8d8a412e493cffac591a643b131bfa47
cymula <amula@cytonn.com>
2023-01-20 18:16:22 +0300

    format-tables

--- src/views/admin/DashBoardView.vue
+++ src/views/admin/DashBoardView.vue
@@ -1,8 +1,24 @@
 <template>
   <h3>Subscriptions</h3>
   <el-table :data="this.subscriptions" border style="width: 100%">
-    <el-table-column prop="created_at" label="Sub Date" width="180" />
-    <el-table-column prop="is_active" label="Sub Status" width="180" />
+    <el-table-column
+      prop="created_at"
+      label="Sub Date"
+      width="180"
+      :formatter="formatDate"
+    >
+      <!-- <template v-slot:prop="prop">
+        {{ new Date(prop.created_at).toLocaleString() }}
+      </template> -->
+    </el-table-column>
+    <el-table-column
+      prop="is_active"
+      label="Sub Status"
+      width="180"
+      :formatter="formatStatus"
+    >
+    </el-table-column>
+
     <el-table-column prop="name" label="Username" />
     <el-table-column prop="email" label="Email" />
     <el-table-column prop="series_name" label="TV Show" />
@@ -17,6 +33,15 @@ export default {
     };
   },
   methods: {
+    formatDate(row) {
+      return new Date(row.created_at).toLocaleString("en-US", {
+        dateStyle: "long",
+        timeStyle: "medium",
+      });
+    },
+    formatStatus(row) {
+      return row.is_active ? "Active" : "Inactive";
+    },
     getSubscriptions() {
       fetch(`http://localhost:4001/api/subscriptions`)
         .then((res) => res.json())
