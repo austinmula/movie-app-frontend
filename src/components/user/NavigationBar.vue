67654294c7203e5b415df45200d8da8c52991317
cymula <amula@cytonn.com>
2023-01-20 16:20:13 +0300

    completed login and logout

--- src/components/user/NavigationBar.vue
+++ src/components/user/NavigationBar.vue
@@ -12,20 +12,55 @@
       <el-menu-item index="homepage">Home</el-menu-item>
       <el-menu-item index="all-series">TV Series</el-menu-item>
       <el-menu-item index="profile">Profile</el-menu-item>
+      <div
+        style="
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 55px;
+          padding-right: 1rem;
+        "
+      >
+        <el-button v-if="currentUser" type="danger" @click="handleLogOut"
+          >Logout</el-button
+        >
+        <el-button v-else type="info" @click="handleLogIn">Login</el-button>
+      </div>
     </el-menu>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import { useRouter } from "vue-router";
+<script>
+import router from "@/router";
+import { mapGetters, mapActions } from "vuex";
 
-const router = useRouter();
+export default {
+  data() {
+    return {
+      activeIndex: "",
+    };
+  },
 
-const activeIndex = ref("1");
-const handleSelect = (key, keyPath) => {
-  console.log(key, keyPath[0]);
-  router.push({ name: keyPath[0] });
+  computed: {
+    ...mapGetters(["current_user"]),
+    currentUser() {
+      return this.current_user.status.loggedIn;
+    },
+  },
+  methods: {
+    ...mapActions(["logout"]),
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath[0]);
+      router.push({ name: keyPath[0] });
+    },
+    handleLogIn() {
+      router.push({ name: "login" });
+    },
+    handleLogOut() {
+      this.logout();
+      router.push({ name: "login" });
+    },
+  },
 };
 </script>
 
