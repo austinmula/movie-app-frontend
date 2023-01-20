064fbada8eb8f2ef9011b2d32a2860d828545550
cymula <amula@cytonn.com>
2023-01-21 02:16:55 +0300

    unsubscribe funtionality complete

--- src/components/user/movies/MovieActions.vue
+++ src/components/user/movies/MovieActions.vue
@@ -1,30 +1,77 @@
 <template>
   <div class="actions-container">
     <div class="flex justify-space-between flex-wrap gap-4">
-      <el-button
+      <el-popover
         v-if="this.isSub.length && this.isSub[0].is_active"
-        type="danger"
-        >Unsubscribe</el-button
+        :visible="visible"
+        placement="top"
+        :width="160"
       >
-      <el-button v-else type="primary">Subscribe</el-button>
+        <p>Are You Sure?</p>
+        <div style="text-align: right; margin: 0">
+          <el-button size="small" text @click="visible = false"
+            >cancel</el-button
+          >
+          <el-button size="small" type="primary" @click="handleUnSub"
+            >confirm</el-button
+          >
+        </div>
+        <template #reference>
+          <el-button type="danger">Unsubscribe</el-button>
+        </template>
+      </el-popover>
 
-      <el-button
-        v-if="this.isfavourite.length && this.isfavourite[0].is_fav"
-        type="danger"
-        >Remove from Favourites</el-button
+      <el-button v-else type="primary" @click="handleSubscribe"
+        >Subscribe</el-button
       >
-      <el-button v-else type="success">Add to Favourites</el-button>
+
+      <el-popover
+        v-if="this.isfavourite.length && this.isfavourite[0].is_fav"
+        :visible="visible"
+        placement="top"
+        :width="160"
+      >
+        <p>Are You Sure?</p>
+        <div style="text-align: right; margin: 0">
+          <el-button size="small" text @click="visible = false"
+            >cancel</el-button
+          >
+          <el-button size="small" type="primary" @click="handleUnFav"
+            >confirm</el-button
+          >
+        </div>
+        <template #reference>
+          <el-button type="danger">Remove from Favourites</el-button>
+        </template>
+      </el-popover>
+
+      <el-button v-else type="success" @click="handleAddToFav"
+        >Add to Favourites</el-button
+      >
+      <el-popover :visible="visible" placement="top" :width="160">
+        <p>Are you sure to delete this?</p>
+        <div style="text-align: right; margin: 0">
+          <el-button size="small" text @click="visible = false"
+            >cancel</el-button
+          >
+          <el-button size="small" type="primary" @click="visible = false"
+            >confirm</el-button
+          >
+        </div>
+      </el-popover>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import axios from "axios";
 export default {
   data() {
     return {
       isfavourite: [],
       isSub: [],
+      url: "http://localhost:4001/api",
     };
   },
   computed: {
@@ -32,6 +79,52 @@ export default {
   },
   props: ["movie_id"],
   methods: {
+    async handleSubscribe() {
+      let user_id = this.current_user.user.user_id;
+      const response = await axios.post(`${this.url}/subscriptions/subscribe`, {
+        user_id: user_id,
+        movie_id: this.movie_id,
+      });
+      if (response.status === 200) {
+        console.log("success");
+        this.isSub = response.data;
+      }
+    },
+    async handleAddToFav() {
+      let user_id = this.current_user.user.user_id;
+      const response = await axios.post(`${this.url}/favourites/add`, {
+        user_id: user_id,
+        movie_id: this.movie_id,
+      });
+      if (response.status === 200) {
+        console.log("success");
+        this.isfavourite = response.data;
+      }
+    },
+    async handleUnFav() {
+      let user_id = this.current_user.user.user_id;
+      const response = await axios.delete(`${this.url}/favourites/remove`, {
+        data: {
+          user_id: user_id,
+          movie_id: this.movie_id,
+        },
+      });
+      if (response.status === 200) {
+        console.log("success");
+        this.isfavourite = [];
+      }
+    },
+    async handleUnSub() {
+      let user_id = this.current_user.user.user_id;
+      const response = await axios.put(`${this.url}/subscriptions/remove`, {
+        user_id: user_id,
+        movie_id: this.movie_id,
+      });
+      // console.log(response);
+      if (response.status === 200) {
+        this.isSub = response.data;
+      }
+    },
     getFavourite() {
       let user_id = this.current_user.user.user_id;
       fetch(`http://localhost:4001/api/favourites/${user_id}/${this.movie_id}`)
